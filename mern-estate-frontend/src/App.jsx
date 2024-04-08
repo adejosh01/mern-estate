980275515855c575ce91dfe15b61a522c31d43c4
Adeloba Joshua <adelobajoshua19@gmail.com>
2024-04-08 20:21:33 +0100

    first commit

---
+++ mern-estate-frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+import Header from './components/Header'
+import Profile from './pages/Profile'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sign-in' element={<Signin />}/>
+        <Route path='/sign-out' element={<SignOut />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>   
+    </BrowserRouter>
+  )
+}
+
+export default App
