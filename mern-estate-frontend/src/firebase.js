9e1dfa06fb08599418ba0fd88f2a04726c6ab467
Adeloba Joshua <adelobajoshua19@gmail.com>
2024-04-09 12:06:30 +0100

    firebase

---
+++ mern-estate-frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQy1xw9cUMAl5JFag6O-QBn34jDuvqc0o",
+  authDomain: "bigjoshestate.firebaseapp.com",
+  projectId: "bigjoshestate",
+  storageBucket: "bigjoshestate.appspot.com",
+  messagingSenderId: "492140480604",
+  appId: "1:492140480604:web:1f717c8b72a42366157e99"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- mern-estate-frontend/src/pages/SignUp.jsx
+++ mern-estate-frontend/src/pages/SignUp.jsx
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
-
-const SignUp = () => {
-  const [formData, setFormData] = useState({})
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
-  const handleChange =(e) => {
+  const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
-    })
-  }
-
+    });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -40,12 +38,10 @@ const SignUp = () => {
       setLoading(false);
       setError(error.message);
     }
-}
-
-
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
@@ -69,24 +65,21 @@ const SignUp = () => {
           onChange={handleChange}
         />
 
-        <button  
+        <button
+          disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-       
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to='/sign-in'>
+        <Link to={'/sign-in'}>
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-      
-   
- 
-  )
+  );
 }
-export default SignUp;
